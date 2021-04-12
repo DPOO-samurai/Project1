82c1bf0d4a8b7e1b392f80fc3d9cc09914d65c6e
orion <idbl64@outlook.com>
2021-04-12 18:47:28 -0500

    Remove unnecessary setters and getters

--- src/main/java/backend/models/Course.java
+++ src/main/java/backend/models/Course.java
@@ -29,54 +29,6 @@ public class Course {
             course.corequisites);
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCredits() {
-        return credits;
-    }
-
-    public void setCredits(int credits) {
-        this.credits = credits;
-    }
-
-    public ArrayList<String> getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(ArrayList<String> attributes) {
-        this.attributes = attributes;
-    }
-
-    public ArrayList<String> getPrerequisites() {
-        return prerequisites;
-    }
-
-    public void setPrerequisites(ArrayList<String> prerequisites) {
-        this.prerequisites = prerequisites;
-    }
-
-    public ArrayList<String> getCorequisites() {
-        return corequisites;
-    }
-
-    public void setCorequisites(ArrayList<String> corequisites) {
-        this.corequisites = corequisites;
-    }
-
     public ArrayList<String> getRequisites() {
         var tmp = new ArrayList<String>();
         tmp.addAll(prerequisites);
